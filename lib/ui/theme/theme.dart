a1767f942558ce3ca2b29ea369ceeb1d29467bcb
Ruan Miniguite <ruanminiguite@gmail.com>
2026-06-07 10:25:30 -0300

    :art: Theme

---
+++ lib/ui/theme/theme.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class ThemeApp {
+  static ThemeData light() {
+    return ThemeData(
+      fontFamily: 'Roboto',
+      appBarTheme: const AppBarTheme(
+        elevation: 0,
+        backgroundColor: Color.fromARGB(200, 175, 135, 100),
+        iconTheme: IconThemeData(
+          color: Colors.white,
+        ),
+        centerTitle: true,
+        titleTextStyle: TextStyle(
+            fontFamily: 'Roboto',
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+            color: Color.fromARGB(255, 255, 255, 255)),
+      ),
+      
+      textTheme: const TextTheme(
+          displayLarge: TextStyle(
+          fontSize: 25,
+          fontWeight: FontWeight.bold,
+          color: Color.fromARGB(255, 255, 255, 255),
+          fontFamily: 'Roboto',
+        ),
+      ),
+      
+      scaffoldBackgroundColor: const Color.fromRGBO(245, 241, 236, 1),
+    );
+  }
+}
